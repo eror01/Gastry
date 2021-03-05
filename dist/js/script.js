fe7e4ff76d653e2c7dbc29d0f25a9abf865aaca0
Eror Aleksandar <eror.aleksandar01@gmail.com>
2021-03-05 16:43:39 +0100

    Added Hamburger menu and responsivnes

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const header = document.querySelector('.header');
+const form = document.querySelector('.book__form')
+
+hamburgerMenu.addEventListener('click', () => {
+    header.classList.toggle('change');
+});
+
+form.addEventListener('submit', (e) => {
+    console.log(e);
+});
